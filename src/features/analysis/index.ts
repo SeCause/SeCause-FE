05d14ff366ce2b6e2bbe786790fafb9679b60bb2
chaeyoungwon <epopcy@naver.com>
2026-08-27 10:06:24 +0900

    feat: 분석 진행 상태 폴링 및 진행 화면 구현

--- src/features/analysis/index.ts
+++ src/features/analysis/index.ts
@@ -1,5 +1,6 @@
-export { useCreateAnalysisRequest } from './hooks/useAnalysisApi';
+export { useAnalysisStatus, useCreateAnalysisRequest } from './hooks/useAnalysisApi';
 export type { AnalysisRepository, AnalysisStep } from './model/types';
+export { default as AnalysisProgress } from './ui/AnalysisProgress';
 export { default as AnalysisSidebar } from './ui/AnalysisSidebar';
 export { default as BranchStep } from './ui/BranchStep';
 export { default as RepoIcon } from './ui/RepoIcon';
